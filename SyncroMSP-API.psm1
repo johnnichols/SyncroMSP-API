8394212a50706ae7d599a946913358aae218507d
John Nichols <jfnichols@gmail.com>
2025-02-09 22:09:53 -0600

    basic put and post requests, with body working

--- SyncroMSP-API.psm1
+++ SyncroMSP-API.psm1
@@ -59,7 +59,7 @@ function Get-SyncroAppointments {
         [int] $page,
 
         [Parameter(Mandatory = $true, ParameterSetName = 'ById')]
-        [int] $id
+        [long] $id
     )
     
     $base_url = "https://$subdomain.syncromsp.com/api/v1"
@@ -72,49 +72,142 @@ function Get-SyncroAppointments {
             $parameters[$key] = $PSBoundParameters[$key] #.ToString()
         }
     }
-    write-host $parameters
-    Write-Host "Parameters: $($parameters.GetEnumerator() | ForEach-Object { "$($_.Key) = $($_.Value)" })"
-    # Debug output to check the type of parameters
-    Write-Host "Type of parameters: $($parameters.GetType().FullName)"
-    $request_url = Format-SyncroRequestUrl -base_url $base_url -endpoint $end_point -parameters $parameters #(Get-Command -Name $MyInvocation.InvocationName).Parameters;
     
-    $token = Get-Secret -name $subdomain -AsPlainText
+    $request = Format-SyncroRequestUrl -base_url $base_url -endpoint $end_point -parameters $parameters -method $method
 
-    $headers = @{
-        "Authorization" = "$token" 
+    # Add body to http request if the format function includes it
+    if ($request.body) {
+        $response = Invoke-RestMethod -Uri $request.request_url -Method $method -body $request.body -Headers @{"Authorization" = Get-Secret -name $subdomain -AsPlainText }
     }
-
-    $response = Invoke-RestMethod -Uri $request_url -Method $method -Headers $headers
-    return $response
+    else {
+        $response = Invoke-RestMethod -Uri $request.request_url -Method $method -Headers @{"Authorization" = Get-Secret -name $subdomain -AsPlainText }
+    }
+    
+    # response object singular or plural
+    if ($response.($end_point.trimStart('/'))) {
+        return $response.($end_point.trimStart('/'))
+    }
+    elseif ($response.($end_point.trimStart('/').trimEnd('s'))) {
+        return $response.($end_point.trimStart('/').trimEnd('s'))
+    }
 }
+
+# WIKI 
+function Add-SyncroWikiPage {
+    param(
+        [Parameter(Mandatory = $true)] [string] $subdomain,
+        
+        [string] $name,
+        [string] $slug,
+        [Parameter(Mandatory = $true)] [string] $body,
+
+        [int] $customer_id,
+        [int] $asset_id,
+        
+        [string] $visibilty    
+    )
+    
+    $base_url = "https://$subdomain.syncromsp.com/api/v1"
+    $end_point = "/wiki_pages"
+    $method = "POST"
+
+    $parameters = @{}
+    foreach ($key in $PSBoundParameters.Keys) {
+        if ($key -ne 'verbose' -and $key -ne 'debug' -and $key -ne 'erroraction' -and $key -ne 'warningaction' -and $key -ne 'informationaction' -and $key -ne 'errorvariable' -and $key -ne 'warningvariable' -and $key -ne 'informationvariable' -and $key -ne 'outbuffer' -and $key -ne 'pipelinevariable') {
+            $parameters[$key] = $PSBoundParameters[$key] #.ToString()
+        }
+    }
+    
+    $request = Format-SyncroRequestUrl -base_url $base_url -endpoint $end_point -parameters $parameters -method $method
+    
+    #write-output ($request.request_url)
+    #write-output ($request.body | ConvertTo-Json -Depth 10)
+    
+    # Add body to http request if the format function includes it
+    if ($request.body) {
+        $response = Invoke-RestMethod -Uri $request.request_url -Method $method -body ($request.body | convertTo-Json) -Headers @{"Authorization" = Get-Secret -name $subdomain -AsPlainText } -ContentType "application/json"
+    }
+    else {
+        $response = Invoke-RestMethod -Uri $request.request_url -Method $method -Headers @{"Authorization" = Get-Secret -name $subdomain -AsPlainText }
+    }
+    
+    # response object singular or plural
+    if ($response.($end_point.trimStart('/'))) {
+        return $response.($end_point.trimStart('/'))
+    }
+    elseif ($response.($end_point.trimStart('/').trimEnd('s'))) {
+        return $response.($end_point.trimStart('/').trimEnd('s'))
+    }
+}
+
+
+
+
+
 
 function Format-SyncroRequestUrl {
     param (
         [Parameter(Mandatory)] [string] $base_url,
         [Parameter(Mandatory)] [string] $endpoint,
         [Parameter(Mandatory)] [hashtable] $parameters,
-        [string] $body
+        [Parameter(Mandatory)] [ValidateSet("GET","PUT","POST","DELETE")] [string] $method
     )   
 
     $key_index = 0
     $request_url = $base_url + $endpoint
-    #Process Parameters
-    foreach ($key in $parameters.keys) {
-
-        $parameter = $parameters[$key] #= Get-Variable -Name $key -ErrorAction SilentlyContinue;
-        if ($null -ne $value -and $value -ne '') {#$parameter -and $parameter.name -and -not [string]::IsNullOrEmpty($parameter.value)) {
-            if ($key_index -eq 0) {
-                $request_url += "?"
-            } else {
-                $request_url += "&"
+    
+    # Put and Post requests will need other paramenters defined in the body
+    if ($method -eq "PUT" -or $method -eq "POST") {
+        
+        $body = @{}
+        foreach ($key in $parameters.keys) {
+            $parameter = $parameters[$key]
+        
+            if ($null -ne $parameter -and $parameter -ne '') {
+                if ($key -eq "id") {
+                    $id = $parameter
+                } elseif ($key -ne "subdomain") {
+                    $body["$($key)"] = $parameter
+                }
+                
             }
-            $request_url += $parameter.name + "=" + $parameter.value
-            #write-host "$($parameter.name) : $($parameter.value)"
         }
 
-        $key_index += 1
     }
-    return $request_url
+    # Process parameters as url queries
+    else {
+        foreach ($key in $parameters.keys) {
+
+            $parameter = $parameters[$key]
+            if ($null -ne $value -and $value -ne '') {
+                if ($key_index -eq 0) {
+                    $request_url += "?"
+                }
+                else {
+                    $request_url += "&"
+                }
+                if ($parameter.name -eq "id") {
+                    $id = $parameter.value
+                }
+                $request_url += $parameter.name + "=" + $parameter.value
+                #write-host "$($parameter.name) : $($parameter.value)"
+            }
+
+            $key_index += 1
+        }
+    }
+
+    
+    if ($id) {
+        $request_url += "/$id"
+    }
+
+    $request = New-Object PsObject -Property @{ 
+        request_url = $request_url;
+        body        = $body;
+    }
+
+    return $request
 
 }
 
